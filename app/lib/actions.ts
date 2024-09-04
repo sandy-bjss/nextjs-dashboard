54970d349359c39b6c2a0d697296f95d56f22ef8
Sandy Lamond <sandy.lamond@bjss.com>
2024-09-04 10:19:28 +0100

    added login page and fixed lint error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -63,7 +65,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 const UpdateInvoice = FormSchema.omit({id:true, date:true});
 
 export async function updateInvoice(id: string, prevState: State, formData: FormData) {
-  const validatedFields = CreateInvoice.safeParse({
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
@@ -110,3 +112,23 @@ export async function deleteInvoice(id: string) {
   }
 }
   
+
+// Authentication
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
